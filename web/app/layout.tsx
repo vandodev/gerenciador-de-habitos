1079ec4d8c08d6047bbafd25261f44720b024e7f
vandodev <vandoaparecido@hotmail.com>
2024-01-19 07:57:26 -0300

    :white_check_mark: :heavy_check_mark: :white_check_mark: Root layout e multiplas fontes

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Dosis } from "next/font/google";
+import { Dosis, Inter } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
 
-const dosis = Dosis({ subsets: ["latin"] });
+const dosis = Dosis({ subsets: ["latin"], variable: "--font-dosis" });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
   title: "Meta diária - Gerenciador de hábitos",
@@ -16,7 +18,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={dosis.className}>{children}</body>
-    </html>
+    <body
+      className={`${dosis.variable} ${inter.variable}flex items-center flex-col mt-10 bg-neutral-900`}
+    >
+      <Image
+        src="/images/logo.svg"
+        width={200}
+        height={200}
+        alt="Logo - meta diária"
+      />
+
+      {children}
+    </body>
+  </html>
+
   );
 }
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <main>
-    <h1 className="text-5xl">Olá mundo</h1>
+    
    </main>
   );
 }
