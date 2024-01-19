f42a783d8ef9b00936176af2f5dc7aeda39ff3fb
vandodev <vandoaparecido@hotmail.com>
2024-01-19 08:17:31 -0300

    :white_check_mark: :heavy_check_mark: :white_check_mark: Menssagem não tem hábitos cadastrados

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,9 +2,15 @@ import { Maiden_Orange } from "next/font/google";
 import Image from "next/image";
 
 export default function Home() {
+  const habits = {}
   return (
-   <main>
-    
+    <main className="container relative flex flex-col gap-8 px-4 pt-16">
+        {habits === null ||
+        (Object.keys(habits).length === 0 && (
+          <h1 className="mt-20 text-4xl font-light text-white font-display text-center">
+            Você não tem hábitos cadastrados
+          </h1>
+        ))}
    </main>
   );
 }
