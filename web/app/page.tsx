681d981b2efbf4799bedc000c5f69677d6900287
vandodev <vandoaparecido@hotmail.com>
2024-01-20 09:15:39 -0300

    :white_check_mark: :heavy_check_mark: :white_check_mark: Exibindo dias da semana

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -16,6 +16,9 @@ export default function Home() {
       '2024-21-01':true,
     },
   }
+
+  const weekDays = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"];
+
   return (
     <main className="container relative flex flex-col gap-8 px-4 pt-16">
         {habits === null ||
@@ -30,12 +33,19 @@ export default function Home() {
         ([habit, habitStreak]) => (
           <div key={habit}>
             <div className="flex justify-between items-center">
-            <span className="text-xl font-light text-white font-sans">
-                {habit}
-            </span>
+              <span className="text-xl font-light text-white font-sans">
+                  {habit}
+              </span>
 
               <button><Image src="/images/trash.svg" width={20} height={20} alt="icone de lixeira para apagar habito"/></button>
             </div>
+
+            <section className="grid grid-cols-7 bg-neutral-800 rounded-md p-2">
+              {/* {['Dom', 'Seg','Terc', 'Qua', 'Qui', 'Sex', 'Sab'].map(day => ( */}
+              {weekDays.map(day => (
+                <span key={day} className="font-sans text-xs text-white text-center">{day}</span>
+              ))}
+            </section>
           </div>
         ))
       }
