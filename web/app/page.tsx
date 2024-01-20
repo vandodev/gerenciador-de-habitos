f8fb0c8fe61c3916c79a9843a06a2a0337b79f2e
vandodev <vandoaparecido@hotmail.com>
2024-01-20 08:38:39 -0300

    :white_check_mark: :heavy_check_mark: :white_check_mark: Exibindo os hábitos

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,8 +1,20 @@
 import { Maiden_Orange } from "next/font/google";
 import Image from "next/image";
+import { json } from "stream/consumers";
 
 export default function Home() {
-  const habits = {}
+  const habits = {
+    'beber água':{
+      '2024-19-01':true,
+      '2024-20-01':false,
+      '2024-21-01':true,
+    },
+    'Estudar programação':{
+      '2024-19-01':true,
+      '2024-20-01':true,
+      '2024-21-01':true,
+    },
+  }
   return (
     <main className="container relative flex flex-col gap-8 px-4 pt-16">
         {habits === null ||
@@ -11,6 +23,15 @@ export default function Home() {
             Você não tem hábitos cadastrados
           </h1>
         ))}
+
+      {habits !== null &&
+      Object.entries(habits).map(
+        ([habit, habitStreak]) => (
+          <p key={habit} className="text-white">
+            {habit} - {JSON.stringify(habitStreak)}
+          </p>
+        ))
+      }
    </main>
   );
 }
