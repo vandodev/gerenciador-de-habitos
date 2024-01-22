2b7e097a48d4e308b3dfc8400c2e1231dcb1c949
vandodev <vandoaparecido@hotmail.com>
2024-01-22 07:23:12 -0300

    :white_check_mark: :heavy_check_mark: :white_check_mark: Formulário para cadastrar novo hábito

--- web/app/novo-habito/page.tsx
+++ web/app/novo-habito/page.tsx
@@ -1,6 +1,31 @@
+
 function NewHabit() {
     return(
-        <h1>Novo hábito</h1>
+        <main className="container relative flex flex-col gap-8 px-12 pt-16">
+            <h1 className="text-4xl font-light text-center text-white font-display">
+                Novo hábito
+            </h1>
+
+            <form  className="flex flex-col gap-4 mt-4">
+                <input
+                    type="text"
+                    name="habit"
+                    id="habit"
+                    className="p-2 font-sans text-xl text-white rounded-md bg-neutral-800"
+                />
+
+                <button
+                    type="submit"
+                    className="bg-[#45EDAD] font-display text-neutral-900 font-regular text-2xl p-2 rounded-md mt-8"
+                    >
+                    cadastrar
+                </button>
+
+                <button className="bg-neutral-800 text-[#F85858] font-display font-regular text-2xl p-2 rounded-md">
+                    cancelar
+                </button>
+            </form>
+        </main>
     )
 }
 
