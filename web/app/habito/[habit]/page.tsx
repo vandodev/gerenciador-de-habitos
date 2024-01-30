746bf2793edb5ddf2c82428d34d8c3114b952a69
vandodev <vandoaparecido@hotmail.com>
2024-01-29 22:36:35 -0300

    :white_check_mark: :heavy_check_mark: :white_check_mark: Componente botão voltar

--- web/app/habito/[habit]/page.tsx
+++ web/app/habito/[habit]/page.tsx
@@ -1,8 +1,29 @@
+import { kv } from "@vercel/kv";
+import Link from "next/link";
+import ArrowIcon from "@/components/ArrowIcon";
 
 async function Habit({ params: { habit } }: { params: { habit: string } }) {
     const decodedHabit = decodeURI(habit);
+    const habitStreak: Record<string, boolean> | null = await kv.hget(
+        "habits",
+        decodedHabit
+      );
+    
     return(
-        <h1 className="text-white">{decodedHabit}</h1>
+        <main className="container relative flex flex-col gap-8 px-12 pt-16">
+        <h1 className="text-2xl font-light text-center text-white font-display">
+          {decodedHabit}
+        </h1>
+  
+        <Link
+          className="flex items-center font-sans text-xs text-neutral-300 gap-2"
+          href="/"
+        >
+          <ArrowIcon width={12} height={12} />
+          Voltar
+        </Link>
+   
+      </main>
     )
 }
 
