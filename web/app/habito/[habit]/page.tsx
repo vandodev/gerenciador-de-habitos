b947e8fcba6ccb1ee6ae4ab4f35c18b32e187683
vandodev <vandoaparecido@hotmail.com>
2024-01-29 23:00:52 -0300

    :white_check_mark: :heavy_check_mark: :white_check_mark: Barra de navegação do calendário

--- web/app/habito/[habit]/page.tsx
+++ web/app/habito/[habit]/page.tsx
@@ -4,13 +4,10 @@ import ArrowIcon from "@/components/ArrowIcon";
 
 async function Habit({ params: { habit } }: { params: { habit: string } }) {
     const decodedHabit = decodeURI(habit);
-    const habitStreak: Record<string, boolean> | null = await kv.hget(
-        "habits",
-        decodedHabit
-      );
+    const habitStreak = await kv.hget("habits", decodedHabit);
     
     return(
-        <main className="container relative flex flex-col gap-8 px-12 pt-16">
+      <main className="container relative flex flex-col gap-8 px-12 pt-16">
         <h1 className="text-2xl font-light text-center text-white font-display">
           {decodedHabit}
         </h1>
@@ -22,6 +19,19 @@ async function Habit({ params: { habit } }: { params: { habit: string } }) {
           <ArrowIcon width={12} height={12} />
           Voltar
         </Link>
+
+        <section className="w-full my-2 rounded-md bg-neutral-800">
+          <div className="flex justify-between mx-2 my-4 font-sans text-neutral-400">
+            <button >
+              <ArrowIcon className="stroke-neutral-400" width={12} height={12} />
+            </button>
+            <span>Janeiro de 2024</span>
+            <button>
+              <ArrowIcon width={12} height={12} className="rotate-180 stroke-neutral-400"/>
+            </button>
+          </div>
+        </section>
+
    
       </main>
     )
